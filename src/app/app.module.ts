8920fb8b7749be9e452f86aff5e1390d8b15fb96
ehueni1982 <barthelemyangora@gmail.com>
2024-10-29 00:18:37 +0100

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { ProduitsComponent } from './produits/produits.component';
 import { NouveauProduitsComponent } from './nouveau-produits/nouveau-produits.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { EditProductComponent } from './edit-product/edit-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProduitsComponent,
     NouveauProduitsComponent,
+    EditProductComponent,
     
   ],
   imports: [
